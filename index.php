199a75121a5de09b24ea5e5ee567323de4ce2584
Sangram <sangrambbsr91@gmail.com>
2023-12-29 09:59:59 +0530

    add file data in action php and jquery

--- index.php
+++ index.php
@@ -103,3 +103,187 @@
 <script src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.12.0/js/dataTables.bootstrap5.min.js"></script>
 
+
+<script>
+
+$(document).ready(function(){
+
+    load_data();
+
+    function load_data()
+    {
+        var seconds = new Date() / 1000;
+
+        $.getJSON("data.json?"+seconds+"", function(data){
+
+            data.sort(function(a,b){
+
+                return b.id - a.id;
+
+            });
+
+            var data_arr = [];
+
+            for(var count = 0; count < data.length; count++)
+            {
+                var sub_array = {
+                    'first_name' : data[count].first_name,
+                    'last_name' : data[count].last_name,
+                    'gender' : data[count].gender,
+                    'image' : data[count].img,
+                    'action' : '<button type="button" class="btn btn-warning btn-sm edit" data-id="'+data[count].id+'">Edit</button>&nbsp;<button type="button" class="btn btn-danger btn-sm delete" data-id="'+data[count].id+'">Delete</button>'
+                };
+
+                data_arr.push(sub_array);
+            }
+
+            $('#sample_data').DataTable({
+                data : data_arr,
+                order : [],
+                columns : [
+                    { data : "first_name" },
+                    { data: "image",
+                        "render": function (data) {
+                            return '<img src="upload/'+data+'"  width="50" height="50"/>';
+                        } 
+                    },
+                    { data : "last_name" },
+                    { data : "gender" },
+                    { data : "action" }
+                ]
+            });
+
+        });
+    }
+
+    $('#add_data').click(function(){
+
+        $('#dynamic_modal_title').text('Add Data');
+
+        $('#sample_form')[0].reset();
+
+        $('#action').val('Add');
+
+        $('#action_button').text('Add');
+
+        $('.text-danger').text('');
+
+        $('#action_modal').modal('show');
+
+    });
+
+    // form submit here--------
+    $('#sample_form').on('submit', function(event){
+
+        event.preventDefault();
+
+        $.ajax({
+            url:"action.php",
+            method:"POST",
+            data:new FormData(this),
+            dataType:"JSON",
+            processData: false,
+            contentType: false,
+            beforeSend:function()
+            {
+                $('#action_button').attr('disabled', 'disabled');
+            },
+            success:function(data)
+            {
+                $('#action_button').attr('disabled', false);
+                if(data.error)
+                {
+                    if(data.error.first_name_error)
+                    {
+                        $('#first_name_error').text(data.error.first_name_error);
+                    }
+                    if(data.error.last_name_error)
+                    {
+                        $('#last_name_error').text(data.error.last_name_error);
+                    }
+                    if(data.error.age_error)
+                    {
+                        $('#age_error').text(data.error.age_error);
+                    }
+                }
+                else
+                {
+                    $('#message').html('<div class="alert alert-success">'+data.success+'</div>');
+
+                    $('#action_modal').modal('hide');
+
+                    $('#sample_data').DataTable().destroy();
+
+                    load_data();
+
+                    setTimeout(function(){
+                        $('#message').html('');
+                    }, 5000);
+                }
+            }
+        });
+
+    });
+
+    $(document).on('click', '.edit', function(){
+
+        var id = $(this).data('id');
+
+        $('#dynamic_modal_title').text('Edit Data');
+
+        $('#action').val('Edit');
+
+        $('#action_button').text('Edit');
+
+        $('.text-danger').text('');
+
+        $('#action_modal').modal('show');
+
+        $.ajax({
+            url:"action.php",
+            method:"POST",
+            data:{id:id, action:'fetch_single'},
+            dataType:"JSON",
+            processData: false,
+            contentType: false,
+            success:function(data)
+            {
+                $('#first_name').val(data.first_name);
+                $('#last_name').val(data.last_name);
+                $('#gender').val(data.gender);
+                $('#age').val(data.age);
+                $('#id').val(data.id);
+            }
+        });
+
+    });
+
+    $(document).on('click', '.delete', function(){
+
+        var id = $(this).data('id');
+        if(confirm("Are you sure you want to delete this data?"))
+        {
+            $.ajax({
+                url:"action.php",
+                method:"POST",
+                data:{action:'delete', id:id},
+                // dataType: "JSON",
+                success:function(data)
+                {
+                    console.log(data);
+                    $('#message').html('<div class="alert alert-success">'+data.success+'</div>');
+                    // $('#sample_data').DataTable().ajax.reload();
+                    $('#sample_data').DataTable().destroy();
+                    load_data();
+                    setTimeout(function(){
+                        $('#message').html('');
+                    }, 5000);
+                }
+            });
+        }
+
+    });
+
+});
+
+</script>
